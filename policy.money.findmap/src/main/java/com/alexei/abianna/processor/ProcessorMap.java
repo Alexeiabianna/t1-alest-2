9d1c9f4a706285844a48cb32bfc3897ed95aa1ee
Alexei Abianna <alexeiabianna@gmail.com>
2024-04-06 19:47:07 -0300

    feat: find bills

--- policy.money.findmap/src/main/java/com/alexei/abianna/processor/ProcessorMap.java
+++ policy.money.findmap/src/main/java/com/alexei/abianna/processor/ProcessorMap.java
@@ -1,7 +1,10 @@
 package com.alexei.abianna.processor;
 
+import com.alexei.abianna.domain.Node;
+import com.alexei.abianna.domain.PathBlocksEnum;
 import com.alexei.abianna.domain.PathGraph;
 import com.alexei.abianna.domain.PathMap;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -23,9 +26,66 @@ public class ProcessorMap {
 
     public void process() {
         pathGraph.getGrid();
+        isFirstBlock(getPathGraph());
+        var block = getFirstBlock(getPathGraph());
+        var nextBlock = getRightBlock(block);
         //TODO insert method that process a search for bills
 
         Logger.getAnonymousLogger().info("Amount bills result: " + pathMap.getAmountBills());
+    }
+
+    private int checkIsBillOnAPath(final char element) {
+        String value = String.valueOf(element);
+        return StringUtils.isNumeric(value) ? Integer.parseInt(value) : -1;
+    }
+
+    private boolean isPathBlock(final char element) {
+        String value = String.valueOf(element);
+        if (PathBlocksEnum.HORIZONTAL_WAY.getValue().equals(value)) {
+            return true;
+        }
+        if (PathBlocksEnum.TURN_LEFT_WAY.getValue().equals(value)) {
+            return true;
+        }
+        if (PathBlocksEnum.TURN_RIGHT_WAY.getValue().equals(value)) {
+            return true;
+        }
+        if (PathBlocksEnum.VERTICAL_WAY.getValue().equals(value)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isFirstBlock(final PathGraph pathGraph) {
+        for(Node[] node : pathGraph.getGrid()) {
+            if(PathBlocksEnum.HORIZONTAL_WAY.getValue().equals(String.valueOf(node[0].getValue()))) {
+                char firstBlock = node[0].getValue();
+                return true;
+            }
+        }
+        return false;
+    }
+    private Node getFirstBlock(final PathGraph pathGraph) {
+        for(Node[] node : pathGraph.getGrid()) {
+            if(PathBlocksEnum.HORIZONTAL_WAY.getValue().equals(String.valueOf(node[0].getValue()))) {
+                return node[0];
+            }
+        }
+        return null;
+    }
+
+    private Node getUpBlock(final Node block) {
+        return block.getNeighbors().get(0);
+    }
+    private Node getDownBlock(final Node block) {
+        return block.getNeighbors().get(1);
+    }
+    private Node getLeftBlock(final Node block) {
+        return block.getNeighbors().get(2);
+    }
+    private Node getRightBlock(final Node block) {
+        return block.getNeighbors().get(3);
     }
 
     public PathGraph getPathGraph() {
